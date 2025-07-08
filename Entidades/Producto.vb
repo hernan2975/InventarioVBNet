1b49bc61920007331210c0d1b0d69ff78df9a51e
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-08 09:49:30 -0300

    Create Producto.vb

---
+++ Entidades/Producto.vb
@@ -0,0 +1,7 @@
+Public Class Producto
+    Public Property Id As Integer
+    Public Property Nombre As String
+    Public Property Stock As Integer
+    Public Property CategoriaId As Integer
+End Class
+
