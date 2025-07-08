3e4b5d77008f3c464aa852ba4d2cea5edb217d06
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-08 09:50:17 -0300

    Create Proveedor.vb

---
+++ Entidades/Proveedor.vb
@@ -0,0 +1,6 @@
+Public Class Proveedor
+    Public Property Id As Integer
+    Public Property Nombre As String
+    Public Property Telefono As String
+    Public Property Email As String
+End Class
