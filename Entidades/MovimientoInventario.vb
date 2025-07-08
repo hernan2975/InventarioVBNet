eeafd0b075c23099789235ea7c0c18dbcdfdc58d
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-08 09:51:11 -0300

    Create MovimientoInventario.vb

---
+++ Entidades/MovimientoInventario.vb
@@ -0,0 +1,8 @@
+Public Class MovimientoInventario
+    Public Property Id As Integer
+    Public Property IdProducto As Integer
+    Public Property Tipo As String ' "Entrada" o "Salida"
+    Public Property Cantidad As Integer
+    Public Property Fecha As Date
+    Public Property Detalle As String
+End Class
